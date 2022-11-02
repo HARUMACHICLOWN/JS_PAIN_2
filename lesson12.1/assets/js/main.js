fdac7bc6a9271524bd3e74db72fd58750ef859a2
Asger <111696192+HARUMACHICLOWN@users.noreply.github.com>
2022-11-02 23:01:18 +0300

    Add files via upload

---
+++ lesson12.1/assets/js/main.js
@@ -0,0 +1,90 @@
+/*let arr = ["Привет", ",","Мир", "!"]
+alert(arr.join(""))*/
+
+/*let text = ["Привет", ",","Мир", "!"]
+alert(text.join(""));
+console.log(text)*/
+
+/*let text = ["Привет","Мир", "!"]
+text[0] = "Пока"
+alert(text)*/
+
+/*let obj = {'Коля':'10000р','Петя':'25700р'};
+alert(obj["Коля"])
+alert(obj["Петя"])*/
+
+/*var arr = {
+    'ru':['голубой','красный','зеленый'],
+    'en':['blue','red','green'],
+};
+alert(arr['ru'][1])*/
+
+
+
+
+/*let arr = ['a', 'b', 'c']
+alert(arr)*/
+
+/*let arr = ['a', 'b', 'c']
+alert(arr[0])
+alert(arr[1])
+alert(arr[2])*/
+
+
+/*var arr = ['a', 'b', 'c', 'd']
+alert(`${arr[0]}+${arr[1]}`) 
+alert(`${arr[2]}+${arr[3]}`)*/
+
+
+/*let arr = [2,5,3,9];
+let result = 2*5 + 3*9;
+alert(result)*/
+
+/*var obj = {a:1, b:2, c:3};
+alert(obj['c']);
+alert(obj.c)*/
+
+/*var obj = {Коля:'1000',Вася:'500', Петя:'200'}
+alert(obj["Коля"])
+alert(obj["Петя"])*/ 
+
+/*var obj = {1:'понедельник',2:'вторник',3:'среда',4:'четверг',5:'пятница',6:'суббота',7:'воскресенье'};
+alert(obj[5])*/
+
+/*var obj = {1:'понедельник',2:'вторник',3:'среда',4:'четверг',5:'пятница',6:'суббота',7:'воскресенье'};
+let day = 3;
+alert(obj[day])*/ 
+
+/*let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]; 
+alert(arr[1][0])*/
+
+/*let obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}; 
+alert(obj.js[0])*/ 
+
+
+/*var ru = {1:'понедельник',2:'вторник',3:'среда',4:'четверг',5:'пятница',6:'суббота',7:'воскресенье'};
+var en = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"};
+alert(ru[1])
+alert(en[3])*/ 
+
+/*var ru = {0: "Понедельник", 1: "Вторник", 2: "Среда", 3: "Четверг", 4: "Пятница", 5: "Суббота", 6: "Воскресенье"}
+var en = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+var lang = prompt('Введите свой язык')
+var day = prompt('Введите свой день недели')
+    if (lang = ru){
+         alert(ru[day-1])
+ }
+    else if (lang = en){
+         alert(en[day-1])
+ }*/ 
+
+
+
+
+
+
+
+
+
+
+
